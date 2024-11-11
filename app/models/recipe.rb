c144311b51c2f62908b48ae0c64f4dbfc7e2bef8
Michael Chaney <mdchaney@michaelchaney.com>
2024-11-10 18:53:53 -0600

    Fixing lint complaints.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
   has_one_attached :image
 
-  normalizes :name, with: -> name { name.strip.downcase.unicode_normalize }
-  normalizes :ingredients, with: -> ingredients { ingredients.strip.downcase.unicode_normalize }
-  normalizes :instructions, with: -> instructions { instructions.strip.downcase.unicode_normalize }
+  normalizes :name, with: ->(name) { name.strip.downcase.unicode_normalize }
+  normalizes :ingredients, with: ->(ingredients) { ingredients.strip.downcase.unicode_normalize }
+  normalizes :instructions, with: ->(instructions) { instructions.strip.downcase.unicode_normalize }
 
   belongs_to :user
 end
