5ceb75cfab66191f7da8f94d638cbd40f8f3c307
Michael Chaney <mdchaney@michaelchaney.com>
2024-11-10 18:47:14 -0600

    Initial commit.

---
+++ db/migrate/20241111000417_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[8.0]
+  def change
+    create_table :recipes do |t|
+      t.references :user, null: false, foreign_key: true, index: true
+
+      t.string :name
+      t.text :ingredients
+      t.text :instructions
+
+      t.timestamps
+    end
+  end
+end
